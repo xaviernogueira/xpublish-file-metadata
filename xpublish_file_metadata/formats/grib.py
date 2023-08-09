8ccb2069a44c31c3e6e3ebee2e1bc7a4cd98cbe5
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-08-09 17:46:21 -0400

    fix: :rotating_light: Hiding eeCodes warning (placeholder for now)

--- xpublish_file_metadata/formats/grib.py
+++ xpublish_file_metadata/formats/grib.py
@@ -1,10 +1,16 @@
-import grib
-import cachey
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', RuntimeWarning)
+    try:
+        import cfgrib
+    except RuntimeError:
+        raise ImportError
 import xarray as xr
+import cachey
 from ..shared import FileMetadata
 
 
-class GridFileMetadata:
+class GribFileMetadata:
     """Get's file metadata for grib."""
 
     def get_file_metadata(
